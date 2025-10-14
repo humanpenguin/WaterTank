9e0d27232276119a09220b0a64a078da8b78a83c
humanpenguin <48625827+humanpenguin@users.noreply.github.com>
2025-10-14 21:31:09 +0100

    Update tank.py

--- tank.py
+++ tank.py
@@ -5,7 +5,9 @@ tank.py:
 
 """
 
-__author__  =   "David A Hall "
+__author__    = "David A Hall "
 __copyright__ = """Copyright 2025, David A Hall
                    HumanPenguin@GMail.com
+                   Licence GPL 3.0 see Licence.md
+		
 """
