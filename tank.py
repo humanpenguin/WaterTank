ef33028ca4b376cc6a29778cfef07b6422ab74c3
humanpenguin <48625827+humanpenguin@users.noreply.github.com>
2025-10-14 21:28:27 +0100

    Create tank.py

---
+++ tank.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+tank.py: 
+  Description 
+
+"""
+
+__author__  =   "David A Hall "
+__copyright__ = """Copyright 2025, David A Hall
+                   HumanPenguin@GMail.com
+"""
